c59069f35f0189bfa1971ff36cbf06cb15a4d9d9
nromano263 <78044734+nromano263@users.noreply.github.com>
2021-02-23 11:06:41 -0600

    Update Python Code.py

--- Python Code.py
+++ Python Code.py
@@ -5,7 +5,7 @@ print('Downloading file request')
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 r = requests.get(url)
 
-with open('/Users/nrcar/Desktop/tcmgfile.txt')
+with open()
     f.write(r.content)
 
 print(r.status_code)
