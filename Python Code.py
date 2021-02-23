5d222e4c5401f66ea9f91fd6fedced0851d79288
Bolotz <78045803+Bolotz@users.noreply.github.com>
2021-02-23 17:11:43 -0600

    Update Python Code.py

--- Python Code.py
+++ Python Code.py
@@ -3,11 +3,10 @@ import requests
 print('Downloading file request')
 
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-r = requests.get(url)
+r = requests.get(url,allow_redirects=True)
 
-with open()
-    f.write(r.content)
+open('logfile.log','wb').write(r.content)
 
 print(r.status_code)
-print(r.headers.['content-type'])
 print(r.encoding)
+
