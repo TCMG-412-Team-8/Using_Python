6c6af0c14b211b3e3116192120e1ef38b7e953f4
nromano263 <78044734+nromano263@users.noreply.github.com>
2021-02-23 10:49:48 -0600

    Add files via upload

--- Python Code.py
+++ Python Code.py
@@ -1,4 +1,13 @@
 import requests
 
+print('Downloading file request')
+
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-r = 
+r = requests.get(url)
+
+with open('/Users/nrcar/Desktop/tcmgfile.txt')
+    f.write(r.content)
+
+print(r.status_code)
+print(r.headers.['content-type'])
+print(r.encoding)
